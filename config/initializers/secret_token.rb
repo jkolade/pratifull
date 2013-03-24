5a17ae745b70753f45aca7e3fe9b1d2f040914ba
johnkolade <johnkolade@johns-MacBook-Pro.local>
2013-03-24 14:29:53 -0500

    this is my first rail application and adding status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pratifull::Application.config.secret_token = 'ecee0de5e5ff3c72a5274c4f75cdde40a50ea95f4015bbd67ef5867a0ca64778f5a5de27b1fceae09ff75529923293b1d45d614de9d698cc17f60f69b32b5161'
